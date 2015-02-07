a62c2a54ef7bbc93f064a93525010f9eaa8f998b
Larry Reid <lcreid@jadesystems.ca>
2015-02-07 08:46:12 -0800

    Adding helper to get JSON result from search
    I added a helper module and class to format the post parameters to call my
    OpenSearchServer server. There's a method to call curl to test it, which
    was working on a simple case and returning a string that looks like JSON.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,44 @@
 include Nanoc::Helpers::LinkTo
 
+require 'json'
+
+module OSS
+  class Search
+    def initialize(query_string)
+      @query_string = query_string
+    end
+    def html
+      "<div>" + @query_string + "</div>"
+    end
+    def parameters_for_post
+      # '     ' +
+      # '{
+      #   "query":"' + @query_string + '",
+      #   "start":0,
+      #   "rows":10,
+      #   "lang":"ENGLISH",
+      #   "returnedFields": [
+      #     "url",
+      #     "backlinkCount"
+      #   ]
+      # }'
+      JSON.pretty_generate( {
+        query: @query_string,
+        start: 0,
+        rows: 10,
+        lang: "ENGLISH",
+        returnedFields: [
+          "url"
+        ]
+      })
+    end
+    def curl_string
+      'curl -XPOST -H "Content-Type: application/json" -d \'' +
+        parameters_for_post +
+        '\' http://opensearchserver01:9090/services/rest/index/Jade/search/pattern'
+    end
+    def curl
+      `#{curl_string}`
+    end
+  end
+end
