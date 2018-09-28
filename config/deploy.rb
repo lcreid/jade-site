129de6916901d631fefe2f84d9aaabf381807075
Larry Reid <lcreid@jadesystems.ca>
2018-09-27 21:40:30 -0700

    Remove files that shouldn't have been generated.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.11.0"
 
 set :application, "jadesystems.ca"
 set :repo_url, "git@github.com:lcreid/jade-site.git"
+set :repo_tree, "_site"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
