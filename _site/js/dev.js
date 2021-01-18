73cff0f1d428c03c30e3ef62126c64690681a221
Larry Reid <lcreid@jadesystems.ca>
2021-01-17 18:26:11 -0800

    Untrack _site

--- _site/js/dev.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  jQuery(function() {
-    window.namespace("ca.jadesystems.search").searchEngine = "http://localhost:8983";
-    return window.namespace("ca.jadesystems.search").solrCore = "cark";
-  });
-
-}).call(this);
